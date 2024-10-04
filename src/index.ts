aceebf60ee804315df4fd6f311278622a074a9c2
Jay <snootched@gmail.com>
2024-10-03 20:22:04 -0400

    css

--- src/index.ts
+++ src/index.ts
@@ -243,12 +243,14 @@ export default class EditorForm extends LitElement {
             }
 
             /* ensure full width for form controls not in two-controls class */
-            .form-row:not(.two-controls) .form-control > * {
-                width: var(--webkit-fill-available);
-            }
+
+            /*.form-row:not(.two-controls) .form-control > * {
+                width: 100% ;
+            }*/
 
             /* Base styles for form controls */
             .form-control {
+                width: auto;
                 display: flex; /* Use flexbox for internal alignment */
                 align-items: center;
                 padding: 8px;
