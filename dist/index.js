90ba99881cd400df3bd7806025ce3467bbad78fc
snootched <snootched@users.noreply.github.com>
2025-05-11 01:08:29 +0000

    Add dist files

--- dist/index.js
+++ dist/index.js
@@ -44,7 +44,6 @@ class EditorForm extends lit_1.LitElement {
     }
     _handleTabActivated(event) {
         this._selectedTab = event.detail.name;
-        console.log("Tab activated:", this._selectedTab);
         this.requestUpdate();
     }
     generateTabs(tabs) {
